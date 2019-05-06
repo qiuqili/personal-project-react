7a87783982888c29a5fece57644447a7dddabe9c
qiuqili <835671347@qq.com>
2019-05-05 21:14:38 -0400

    Set up basic structure :paw_prints:

---
+++ src/EventLists.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EventLists = () => {
+  return (
+    <section>
+      <h2>Lists here</h2>
+    </section>
+  );
+};
+
+export default EventLists;
---
+++ src/LoginForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginForm = () => {
+  return (
+    <section>
+      <h2>Please enter your Github username:</h2>
+    </section>
+  );
+};
+
+export default LoginForm;
